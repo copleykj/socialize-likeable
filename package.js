bcfb803f839fdd0b89b0ae9c3347230698d11398
Kelly <copleykj@gmail.com>
2015-04-24 13:21:38 -0400

    add a version constraint for linkable-model

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "meteor", "mongo", "underscore", "socialize:base-model@0.1.2", "socialize:linkable-model", "tmeasday:publish-with-relations@0.2.0",
+        "meteor", "mongo", "underscore", "socialize:base-model@0.1.2", "socialize:linkable-model@0.1.0", "tmeasday:publish-with-relations@0.2.0",
         "aldeed:simple-schema@1.3.0", "aldeed:collection2@2.3.2", "matb33:collection-hooks@0.7.9",
         "meteorhacks:unblock@1.1.0"
     ]);
