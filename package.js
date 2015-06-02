28c3463c27e4f5997c95e3c9aceb43f60308a3f9
Kelly <copleykj@gmail.com>
2015-06-02 12:00:17 -0400

    remove deps that the new base-model includes by default

--- package.js
+++ package.js
@@ -7,11 +7,9 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
-    api.use([
-        "meteor", "mongo", "underscore", "socialize:base-model@0.1.2", "socialize:linkable-model@0.1.0", "tmeasday:publish-with-relations@0.2.0",
-        "aldeed:simple-schema@1.3.0", "aldeed:collection2@2.3.2", "matb33:collection-hooks@0.7.9",
-        "meteorhacks:unblock@1.1.0"
-    ]);
+    api.use("socialize:linkable-model@0.2.0");
+
+    api.imply("socialize:linkable-model");
 
     //Add the friend-model files
     api.addFiles("common/likeable-model.js");
