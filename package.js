4d5cd211b76010b9d9a3e0fde0ae846f2e190eb8
copleykj <copleykj@gmail.com>
2016-02-12 13:05:56 -0500

    patch version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:likeable",
     summary: "A package implementing social \"liking\" or \"starring\"",
-    version: "0.2.0",
+    version: "0.2.1",
     git: "https://github.com/copleykj/socialize-likeable.git"
 });
 
