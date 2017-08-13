4abf8b2b08eb4b36b477cbebe73fb53c4a2d3860
Kelly Copley <copleykj@gmail.com>
2017-08-13 15:28:00 -0400

    user getCollectionForParentLink instead of old api

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ LikesCollection.allow({
 
 LikesCollection.after.insert(function afterInsert(userId, like) {
     // after a successful like, increment the linked object's _likeCount property
-    const collection = LinkableModel.getCollectionForRegisteredType(like.objectType);
+    const collection = this.transform().getCollectionForParentLink();
     userId && collection && collection.update(like.linkedObjectId, { $inc: { _likeCount: 1 } });
 });
 
 LikesCollection.after.remove(function afterRemove(userId, like) {
     // if the user unlikes an object, decrement the linked objects _likeCount property
-    const collection = LinkableModel.getCollectionForRegisteredType(like.objectType);
+    const collection = this.transform().getCollectionForParentLink();
     userId && collection && collection.update(like.linkedObjectId, { $inc: { _likeCount: -1 } });
 });
