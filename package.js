2442b1341b034aae5349f11b018ba4be0aaefe51
StorytellerCZ <storyteller@freedombase.net>
2018-01-26 18:47:40 +0100

    Fix dependencies

--- package.js
+++ package.js
@@ -9,9 +9,9 @@ Package.describe({
 Package.onUse(function _(api) {
   api.versionsFrom('1.3');
 
-  api.use(['socialize:linkable-model@1.0.0', 'socialize:user-model@1.0.0', 'reywood:publish-composite@1.5.2']);
+  api.use(['socialize:user-model@1.0.0', 'reywood:publish-composite@1.5.2']);
 
-  api.imply('socialize:linkable-model');
+  api.imply(['socialize:linkable-model', 'socialize:user-model']);
 
   api.mainModule('server/server.js', 'server');
   api.mainModule('common/common.js');
