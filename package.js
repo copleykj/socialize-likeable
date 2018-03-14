b8ca1809a431703bcddab43b1bd36aaf3b003840
StorytellerCZ <storyteller@freedombase.net>
2018-03-14 10:51:22 +0100

    Update user-model dependency

--- package.js
+++ package.js
@@ -2,14 +2,14 @@
 Package.describe({
     name: 'socialize:likeable',
     summary: 'A package implementing social "liking" or "starring"',
-    version: '1.0.0',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-likeable.git',
 });
 
 Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
-    api.use(['socialize:user-model@1.0.0', 'reywood:publish-composite@1.5.2']);
+    api.use(['socialize:user-model@1.0.2', 'reywood:publish-composite@1.5.2']);
 
     api.imply('socialize:user-model');
 
