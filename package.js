ea7e21b0d20a0a7a53bf3aa8ed1bf46d7333cff8
Kelly Copley <copleykj@gmail.com>
2018-04-20 13:13:58 -0400

    remove unnecessary user-model dep
    We need the user blocking package for publications which implies the
    user-model package so this dependency is not necessary.

--- package.js
+++ package.js
@@ -10,9 +10,8 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'socialize:user-model@1.0.0',
-        'reywood:publish-composite@1.5.2',
         'socialize:user-blocking@1.0.0',
+        'reywood:publish-composite@1.5.2',
     ]);
 
     api.imply('socialize:user-model');
