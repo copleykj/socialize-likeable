1b867d2bf6f36d94b9bf9ee46891dbdce75944e7
Kelly Copley <copleykj@gmail.com>
2018-05-26 23:05:27 -0400

    update deps

--- package.js
+++ package.js
@@ -10,8 +10,8 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'socialize:user-blocking@1.0.0',
-        'reywood:publish-composite@1.5.2',
+        'socialize:user-blocking@1.0.1',
+        'reywood:publish-composite@1.6.0',
     ]);
 
     api.imply('socialize:user-blocking');
