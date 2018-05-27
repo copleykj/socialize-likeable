a0eca727f207f71fa91ab35ff3b8b5171f58e2bb
Kelly Copley <copleykj@gmail.com>
2018-05-26 23:05:59 -0400

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:likeable',
     summary: 'A package implementing social "liking" or "starring"',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-likeable.git',
 });
 
