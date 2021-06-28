a092f4ae25de981c8e435aca3e4ef210f6dffd17
Kelly Copley <copleykj@gmail.com>
2021-06-27 23:54:48 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -2,15 +2,15 @@
 Package.describe({
     name: 'socialize:likeable',
     summary: 'A package implementing social "liking" or "starring"',
-    version: '1.0.4',
+    version: '1.0.5',
     git: 'https://github.com/copleykj/socialize-likeable.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
 
     api.use([
-        'socialize:user-blocking@1.0.5',
+        'socialize:user-blocking@1.0.6',
         'reywood:publish-composite@1.7.3',
     ]);
 
